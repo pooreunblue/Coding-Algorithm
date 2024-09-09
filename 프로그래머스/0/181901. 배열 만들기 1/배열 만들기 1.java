5b4cb53c314f73afd07e8e75791c4e1bc6a236e5
pooreunblue <97025669+pooreunblue@users.noreply.github.com>
2024-09-09 20:16:37 +0900

    [level 0] Title: 배열 만들기 1, Time: 16.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i=k;i<=n;i++) {
+            if(i%k==0)
+                answer.add(i);
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
