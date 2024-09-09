d7744f983a4c173b6b912ee12c326c4e41e304b8
pooreunblue <97025669+pooreunblue@users.noreply.github.com>
2024-09-09 16:11:58 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        char[] arr = my_string.toCharArray();
+        while(s<e) {
+                char tmp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = tmp;
+                
+                s++;
+                e--;
+            }
+        return new String(arr);
+    }
+}
