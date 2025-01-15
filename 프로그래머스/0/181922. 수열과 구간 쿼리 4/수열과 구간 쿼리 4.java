764bf950c3085e54f1811e73e1b3f8b9a5d4884d
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-01-16 01:46:37 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.86 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+          
+        for(int j=0; j<queries.length; j++) {
+            for(int i = queries[j][0]; i <= queries[j][1]; i++) {
+                if(i % queries[j][2] == 0) {
+                    arr[i]++;
+                }  
+            }
+        }
+        return arr;
+    }
+}
