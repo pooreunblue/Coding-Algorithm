32a6eccf0e683d689bb99e778e9476a94a334f54
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-02-05 19:20:44 +0900

    [level 0] Title: 배열 만들기 6, Time: 46.55 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        for(int i=0; i<arr.length; i++) {
+            if(stk.size() == 0) {
+                stk.add(arr[i]);
+            } else if(stk.get(stk.size()-1) == arr[i]) {
+                stk.remove(stk.size()-1);
+            } else if(stk.get(stk.size()-1) != arr[i]) {
+                stk.add(arr[i]);
+            }
+        }
+        if(stk.size() == 0) {
+            return new int[]{-1};
+        }
+        return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
