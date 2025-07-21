b73a955c62ce21d0e2c6e4a5a4155cebaf573a9b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-07-21 17:52:21 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,8 +1,5 @@
 def solution(price):
-    if price >= 500000:
-        price *= 0.8
-    elif price >= 300000:
-        price *= 0.9
-    elif price >= 100000:
-        price *= 0.95
-    return int(price)
+    discount_list = {500000 : 0.8, 300000 : 0.9, 100000 : 0.95, 0 : 1}
+    for discount_price, discount_rate in discount_list.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
