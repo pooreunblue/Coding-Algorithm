3be6ce1d922e4f268658b5a1a537e9d7415ac2f6
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-19 11:18:22 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,3 @@
 def solution(age):
-    result=[]
     dict = {'0':'a','1':'b','2':'c','3':'d','4':'e','5':'f','6':'g','7':'h','8':'i','9':'j'}
-    age = str(age)
-    for i in age:
-        result.append(dict.get(i))
-    return "".join(result)
+    return "".join(dict[i] for i in str(age))
