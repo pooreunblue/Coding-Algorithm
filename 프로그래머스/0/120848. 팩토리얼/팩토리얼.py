71aaa51d2e08d6f6d6c2de00109ea38151041444
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-26 15:15:37 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,10 +1,8 @@
-import math
-
 def solution(n):
-    list = [math.factorial(i) for i in range(1,11)]
-    for i in list:
-        if i>n:
-            return list.index(i)
-        elif i==n:
-            return list.index(i)+1
-        
+    answer = 1
+    factorial = 1
+    while factorial <= n:
+        answer += 1
+        factorial *= answer
+    answer-=1
+    return answer
