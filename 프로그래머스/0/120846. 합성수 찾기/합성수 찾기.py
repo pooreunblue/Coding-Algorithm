eb1719518df84753203a43a81084647161be20fd
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-26 13:49:46 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,11 +1,9 @@
 def solution(n):
     count = 0
     for i in range(4, n+1):
-        divisor_count = 0
-        for j in range(1, i+1):
+        for j in range(2, int(i**0.5)+1):
             if i%j == 0:
-                divisor_count += 1
-        if divisor_count >= 3:
-            count += 1
+                count += 1
+                break
     return count
         
