bd88ebc907c452f181b7aa1a84c8d2e949db2870
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-27 23:25:25 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,5 @@
 def solution(my_string):
     vowels = ['a','i','o','e','u']
-    for i in my_string:
-        for j in vowels:
-            if i == j:
-                my_string = my_string.replace(i,'')
+    for vowel in vowels:
+        my_string = my_string.replace(vowel,'')
     return my_string
