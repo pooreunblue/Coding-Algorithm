82883b0ebf76f5be56d76704706d4bb418fbe653
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-31 14:34:57 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,7 +1,4 @@
 def solution(my_string, num1, num2):
     char_list = list(my_string)
-    c1 = char_list[num1]
-    c2 = char_list[num2]
-    char_list[num1] = c2
-    char_list[num2] = c1
+    char_list[num1], char_list[num2] = char_list[num2], char_list[num1]
     return ''.join(char_list)
