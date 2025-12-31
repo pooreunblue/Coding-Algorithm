c41e59ae592a38ce922067ddbfb1cf144ccb5086
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-31 11:07:54 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,8 +1,6 @@
 def solution(order):
     count = 0
     clap_num = ['3','6','9']
-    order_num = str(order)
-    for i in order_num:
-        if i in clap_num:
-            count += 1
+    for i in clap_num:
+        count += str(order).count(i)
     return count
