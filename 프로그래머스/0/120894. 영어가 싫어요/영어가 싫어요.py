7d782d075769aa0dda46a4628b398abe05081b28
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-31 13:26:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    replace_map = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for k, v in replace_map.items():
+        numbers = numbers.replace(k, v)
+    return int(numbers)
