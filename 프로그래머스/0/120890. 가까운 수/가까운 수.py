fa7a7c314a3a0028097c11469611f8b955b2459d
백승호 <97025669+pooreunblue@users.noreply.github.com>
2025-12-31 09:23:28 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,9 @@
 def solution(array, n):
     min_diff = 99
     near = 0
+    array.sort()
     for i in array:
         if min_diff > abs(i-n):
             min_diff = abs(i-n)
             near = i
-        elif min_diff == abs(i-n):
-            near = min(near, i)
     return near
