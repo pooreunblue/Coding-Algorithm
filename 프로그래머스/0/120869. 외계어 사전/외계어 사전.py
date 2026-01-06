844d43e9b72f4fe3dc74113122e0b26074c15d9f
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-06 16:07:37 +0900

    [level 0] Title: 외계어 사전, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,13 +1,6 @@
 def solution(spell, dic):
-    ans = 1
-    arr = []
     for s in dic:
-        for c in spell:
-            if s.count(c) != 1:
-                s = s.replace(s,'')
-        if s != '':
-            arr.append(s)
-    if len(arr) < 1:
-        ans += 1
-    return ans
+        if sorted(spell) == sorted(s):
+            return 1
+    return 2
     
