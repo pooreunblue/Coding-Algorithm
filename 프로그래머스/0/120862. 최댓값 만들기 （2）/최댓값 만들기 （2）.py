957390938f8e74f8efd56fe948e96c2a93dfaa7d
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-06 09:29:42 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,9 +1,3 @@
 def solution(numbers):
     numbers = sorted(numbers)
-    negative = sum(1 for n in numbers if n < 0)
-    if negative < 1:
-        return numbers[-2] * numbers[-1]
-    else:
-        if numbers[0] * numbers[1] < numbers[-2] * numbers[-1]:
-            return numbers[-2] * numbers[-1]
-        else: return numbers[0] * numbers[1]
+    return max(numbers[0] * numbers[1], numbers[-2] * numbers[-1])
