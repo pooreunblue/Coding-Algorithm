6ce4abbf46ae86f60d1de8effac112b209369bc7
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-13 14:26:06 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,11 @@
+h, m = map(int, input().split())
+if h == 0:
+    if m >= 45:
+        print(0, m-45)
+    else:
+        print(23, m+15)
+else:
+    if m >= 45:
+        print(h, m-45)
+    else:
+        print(h-1, m+15)
