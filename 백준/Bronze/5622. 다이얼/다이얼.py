e525cd6ec4f67aad68a6f715d07c1cb453858fc5
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-19 14:55:26 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,21 +1,8 @@
-w = input()
+words = input()
+dials = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 a = 0
-for i in w:
-    if i in 'ABC':
-        a += 2
-    elif i in 'DEF':
-        a += 3
-    elif i in 'GHI':
-        a += 4
-    elif i in 'JKL':
-        a += 5
-    elif i in 'MNO':
-        a += 6
-    elif i in 'PQRS':
-        a += 7
-    elif i in 'TUV':
-        a += 8
-    else:
-        a += 9
-a += len(w)
+for w in words:
+    for i in range(len(dials)):
+        if w in dials[i]:
+            a += i+3
 print(a)
