b467d26edf9310662e01660dadb354e8ee186147
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-29 12:55:17 +0900

    [Bronze I] Title: 단어 공부, Time: 148 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,9 +1,8 @@
-s = list(input().upper())
+s = input().upper()
 dic = {}
-for i in set(s):
-    dic[i] = s.count(i)
-for key, value in dic.items():
-    m = [k for k, v in dic.items() if v == max(dic.values())]
-if len(m) ==  1:
-    print(*m)
-else: print('?')
+for i in s:
+    dic[i] = dic.get(i, 0) + 1
+m = [k for k, v in dic.items() if v == max(dic.values())]
+if len(m) > 1:
+    print('?')
+else: print(m[0])
