edee762d0432ccd9774b3d4f23295fb61f526b31
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-01-30 11:44:42 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -4,6 +4,5 @@ for _ in range(n):
     x, y = map(int, input().split())
     for i in range(x, x+10):
         for j in range(y, y+10):
-            sq[i][j] = 1
-count = sum(row.count(1) for row in sq)
-print(count)
+            sq[i][j] = 1 
+print(sum(row.count(1) for row in sq))
