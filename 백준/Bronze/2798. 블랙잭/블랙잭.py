c47301b7af46af26913782ac4dbd9e884a1d923c
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-02-12 09:34:25 +0900

    [Bronze II] Title: 블랙잭, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,8 +2,5 @@ from itertools import combinations
 
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
-sums = []
-for i in combinations(nums, 3):
-    sums.append(sum(i))
-result = max([s for s in sums if s <= m], default=None)
+result = max(sum(n) for n in combinations(nums, 3) if sum(n) <= m)
 print(result)
