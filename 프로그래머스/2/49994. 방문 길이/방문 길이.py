73b350309ac00bbc910c5b39fd96b6f2aecc214b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-11 15:09:12 +0900

    [level 2] Title: 방문 길이, Time: 0.25 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,26 +1,11 @@
-def is_valid(nx, ny):
-    return 0 <= nx <= 10 and 0 <= ny <= 10
-
-def move(x, y, dir):
-    if dir == 'U':
-        nx, ny = x, y+1
-    elif dir == 'D':
-        nx, ny = x, y-1
-    elif dir == 'R':
-        nx, ny = x+1, y
-    elif dir == 'L':
-        nx, ny = x-1, y
-    return nx, ny
-        
 def solution(dirs):
-    x, y = 5, 5
+    x, y = 0, 0
     path = set()
+    moving = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
     for dir in dirs:
-        nx, ny = move(x, y, dir)
-        if not is_valid(nx, ny):
-            continue
-        path.add((x,y,nx,ny))
-        path.add((nx,ny,x,y))
-        x, y = nx, ny
+        nx, ny = x + moving[dir][0], y + moving[dir][1]
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            path.add((x,y,nx,ny))
+            path.add((nx,ny,x,y))
+            x, y = nx, ny
     return len(path)/2
-    
