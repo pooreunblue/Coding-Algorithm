23db77302f7c77d11a879a1c9a44662c87aa874c
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-12 11:34:21 +0900

    [level 1] Title: 실패율, Time: 19.47 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,22 +1,4 @@
 def solution(N, stages):
-    fail = []
-    r = []
-    for i in range(1,N+1):
-        if i not in stages:
-            fail.append(0)
-        else:
-            fail.append(stages.count(i)/len(stages))
-        stages = [j for j in stages if j != i]
-    n = len(fail)
-    while len(r) < N:
-        max_fail = max(fail)
-        for i, x in enumerate(fail):
-            if x == max_fail:
-                r.append(i+1)
-                fail[i] = -1
-    return r
-
-def solution(N, stages):
     unclear = [0] * (N+2)
     for stage in stages:
         unclear[stage] += 1
