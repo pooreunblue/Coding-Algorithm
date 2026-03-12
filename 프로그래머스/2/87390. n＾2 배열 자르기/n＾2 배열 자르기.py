612188e959930c846d73e4520f5261d76010f22f
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-12 10:52:58 +0900

    [level 2] Title: n^2 배열 자르기, Time: 36.55 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,21 +1,2 @@
 def solution(n, left, right):
-    arr = []
-    for i in range(left, right+1):
-        k = max(i // n, i % n)
-        arr.append(k+1)
-    return arr
-
-#   0 1 2
-# 0 1 2 3
-# 1 2 2 3
-# 2 3 3 3
-
-# 0 1 2 3 4 5 6 7 8
-# 1 2 3 2 2 3 3 3 3
-
-#   0 1 2 3 4 
-# 0 1 2 3 4 5
-# 1 2 2 3 4 5
-# 2 3 3 3 4 5
-# 3 4 4 4 4 5
-# 4 5 5 5 5 5
+    return [max(i//n,i%n)+1 for i in range(left, right+1)]
