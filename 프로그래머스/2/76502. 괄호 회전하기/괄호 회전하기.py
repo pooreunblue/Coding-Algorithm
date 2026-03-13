1ac37e922ee84b5bc0703c08a3841a46f4969d18
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-13 15:25:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 81.34 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,30 @@
+def solution(s):
+    cnt = 0
+    for _ in range(len(s)):
+        stk = []
+        is_correct = False
+        for c in s:
+            if c == '[' or c == '(' or c == '{':
+                stk.append(c)
+            elif c == ']':
+                if not stk or stk[-1] != '[':
+                    is_correct = False
+                    break
+                stk.pop()
+                is_correct = True
+            elif c == ')':
+                if not stk or stk[-1] != '(':
+                    is_correct = False
+                    break
+                stk.pop()
+                is_correct = True
+            elif c == '}':
+                if not stk or stk[-1] != '{':
+                    is_correct = False
+                    break
+                stk.pop()
+                is_correct = True
+        if not stk and is_correct:
+            cnt += 1
+        s = s[1:] + s[0]
+    return cnt
