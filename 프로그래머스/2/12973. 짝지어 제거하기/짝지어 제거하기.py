c75149494b791e9714f9904adddb190719be5ba4
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-13 16:14:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 90.79 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,12 +1,8 @@
 def solution(s):
     stk = []
     for c in s:
-        if not stk:
-            stk.append(c)
-        elif stk[-1] == c:
+        if stk and stk[-1] == c:
             stk.pop()
         else:
             stk.append(c)
-    if not stk:
-        return 1
-    return 0
+    return 1 if not stk else 0
