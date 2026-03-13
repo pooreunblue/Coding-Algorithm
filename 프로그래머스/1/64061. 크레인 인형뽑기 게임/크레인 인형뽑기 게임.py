39e1651d04b4da20402bd9794298daed6d2cc58e
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-13 22:17:11 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.38 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    n = len(board)
+    cnt = 0
+    stks = [[] * n for _ in range(n)]
+    basket = []
+    for i, row in enumerate(reversed(board)):
+        for j in range(n):
+            if row[j]:
+                stks[j].append(row[j])
+    for m in moves:
+        if stks[m-1] and stks[m-1][-1]:
+            basket.append(stks[m-1].pop())
+            if len(basket) >= 2 and basket[-1] == basket[-2]:
+                basket.pop()
+                basket.pop()
+                cnt += 2
+    return cnt
