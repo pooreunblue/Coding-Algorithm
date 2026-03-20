f9f604f692487fd7443a4fdbb15eb78c79642cee
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-20 16:16:13 +0900

    [Gold V] Title: Z, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -6,24 +6,20 @@ def Z(x,y,N):
     cnt = 0
     if N == 1:
         if x == 0 and y == 0:
-            pass
+            return 0
         elif x == 0 and y == 1:
-            cnt += 1
+            return 1
         elif x == 1 and y == 0:
-            cnt += 2
-        else: cnt += 3
-        return cnt
+            return 2
+        else: return 3
     else:
         if x < 2**(N-1) and y < 2**(N-1):
             return Z(x, y, N-1)
         elif x < 2**(N-1) and 2**(N-1) <= y < 2**N:
-            cnt += 2**(2*N-2)
-            return cnt + Z(x,y-2**(N-1), N-1)
+            return 2**(2*N-2) + Z(x,y-2**(N-1), N-1)
         elif 2**(N-1) <= x < 2**N and y < 2**(N-1):
-            cnt += 2**(2*N-1)
-            return cnt + Z(x-2**(N-1), y, N-1)
-        else:
-            cnt += (2**(2*N-2))*3
-            return cnt + Z(x-2**(N-1), y-2**(N-1), N-1)
+            return 2**(2*N-1) + Z(x-2**(N-1), y, N-1)
+        else: 
+            return (2**(2*N-2))*3 + Z(x-2**(N-1), y-2**(N-1), N-1)
         
 print(Z(r,c,N))
