a3be3d0672e10f73e6e01dc137202ed4e627e2df
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-20 22:36:24 +0900

    [level 2] Title: 할인 행사, Time: 184.33 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    hash = {}
+    cnt = 0
+    for i in range(len(want)):
+        hash[want[i]] = number[i]
+    for i in range(len(discount)-9):
+        goods = Counter(discount[i:i+10])
+        for h in hash.keys():
+            if goods[h] != hash[h]:
+                break
+            else: continue
+        else:
+            print(i)
+            cnt += 1
+    return cnt
