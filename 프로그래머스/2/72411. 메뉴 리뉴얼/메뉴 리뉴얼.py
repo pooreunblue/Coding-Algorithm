b5d1018b9f42184772ad8beea7389363c9f8881b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-29 21:30:09 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 6.24 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -8,19 +8,18 @@ def solution(orders, course):
         for c in course:
             if len(order) >= c:
                 menus = [''.join(menu) for menu in combinations(sorted(order),c)]
+                counts[c] = ('',0)
                 for menu in menus:
                     if menu not in courses:
                         courses[menu] = 1
                     else:
                         courses[menu] += 1
+    print(courses)
     for c in course:
         for menu, count in courses.items():
             length = len(menu)
             if length == c and count > 1:
-                if length in counts:
-                    if count > counts[length][1]:
-                        counts[length] = (menu, count)
-                else:
+                if count > counts[length][1]:
                     counts[length] = (menu, count)
     for length, values in counts.items():
         for menu, count in courses.items():
