d9de623eca49af9da5d15e04506c7aefcd34b3bb
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-03-30 16:46:16 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 106.74 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,28 +1,18 @@
 from itertools import combinations
+from collections import Counter
 
 def solution(orders, course):
     answer = []
-    courses = {}
-    counts = {}
-    for order in orders:
-        for c in course:
-            if len(order) >= c:
-                menus = [''.join(menu) for menu in combinations(sorted(order),c)]
-                counts[c] = ('',0)
-                for menu in menus:
-                    if menu not in courses:
-                        courses[menu] = 1
-                    else:
-                        courses[menu] += 1
-    print(courses)
     for c in course:
-        for menu, count in courses.items():
-            length = len(menu)
-            if length == c and count > 1:
-                if count > counts[length][1]:
-                    counts[length] = (menu, count)
-    for length, values in counts.items():
-        for menu, count in courses.items():
-            if len(menu) == length and count == values[1]:
-                answer.append(menu)
+        menus = []
+        for order in orders:
+            comb = combinations(sorted(order),c)
+            menus += comb
+
+        counts = Counter(menus)
+        if len(counts) and max(counts.values()) != 1:
+            for menu, count in counts.items():
+                if count == max(counts.values()):
+                    answer.append(''.join(menu))
+
     return sorted(answer)
