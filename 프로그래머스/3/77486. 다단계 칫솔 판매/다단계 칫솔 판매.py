a4e9dc14dd0dd077746fe609d0aa05e561f4150b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-09 11:01:56 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 107.37 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,11 +1,11 @@
 def solution(enroll, referral, seller, amount):
     answer = [0] * len(enroll)
     recommend = dict(zip(enroll, referral))
-    index = {val:i for i, val in enumerate(enroll)}
+    answer = {member: 0 for member in enroll}
     for i, s in enumerate(seller):
         profits = amount[i] * 100
         while profits > 0 and s != '-':
-            answer[index[s]] += profits - profits // 10
+            answer[s] += profits - profits // 10
             profits //= 10
             s = recommend[s]
-    return answer
+    return [answer[member] for member in enroll]
