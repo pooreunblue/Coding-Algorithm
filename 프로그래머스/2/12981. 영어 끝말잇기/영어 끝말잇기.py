165e128d7c8c928c32c0b79d3f44404c7026a94b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-15 10:00:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,21 +1,9 @@
-def add(arr, i, n):
-    arr[0] = i % n + 1
-    arr[1] = (i + n) // n
-    return arr
 def solution(n, words):
     word_set = set()
-    answer = [0,0]
     word_set.add(words[0])
     for i in range(1, len(words)):
-        if i > 0:
-            w = words[i-1][-1]
-            if words[i].startswith(w):
-                if words[i] not in word_set:
-                    word_set.add(words[i])
-                else:
-                    add(answer, i, n)
-                    break
-            else:
-                add(answer, i, n)
-                break
-    return answer
+        pre_word = words[i-1][-1]
+        if words[i] in word_set or words[i][0] != pre_word: 
+            return [i % n + 1, i // n + 1]
+        word_set.add(words[i])
+    return [0,0]
