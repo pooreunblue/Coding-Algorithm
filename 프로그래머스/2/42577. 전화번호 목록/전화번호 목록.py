f1fec5c36cfa7cb60643444f7f8cfccae39785ed
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-15 10:38:13 +0900

    [level 2] Title: 전화번호 목록, Time: 96.53 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,7 @@
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        prefix = phone_book[i]
+        if phone_book[i+1].startswith(prefix):
+            return False
+    return True
