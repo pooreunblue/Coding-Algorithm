d778767472241ca0ceedc4ed584cada1ccdb8652
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-20 21:44:38 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 616 ms, Memory: 81656 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,12 +1,26 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(10 ** 6)
+
+adj_list = defaultdict(list)
+visited = set()
+N, M, R = map(int, sys.stdin.readline().split())
+order = [0] * (N+1)
+i = 1
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+
+def dfs(node):
+    visited.add(node)
+    order[node] = len(visited)
+    for neighbor in adj_list[node]:
+        if neighbor not in visited:
+            dfs(neighbor)
+            
+for node in adj_list:
+    adj_list[node].sort()
+dfs(R)
+for i in order[1:]:
+    print(i)
