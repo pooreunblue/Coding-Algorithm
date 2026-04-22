00ba7839373142bfc031cea5e197ad0617814753
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-22 22:10:23 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.83 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 0.38 ms
+메모리: 9.08 MB, 시간: 1.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 10일 14:56:56
+2026년 04월 22일 22:10:19
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,4 +1,7 @@
 def solution(numbers):
-    sums = set([(numbers[i]+numbers[j]) for i in range(len(numbers)) for j in range(i+1, len(numbers))])
-    sorted_list = sorted(list(sums))
-    return sorted_list
+    sum = set()
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j:
+                sum.add(numbers[i]+numbers[j])
+    return sorted(list(sum))
