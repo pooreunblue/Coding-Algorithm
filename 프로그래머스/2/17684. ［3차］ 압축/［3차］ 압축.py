c85c7f8ba5edd1a6f00dd0332143227c7ca3be25
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-22 14:44:58 +0900

    [level 2] Title: [3차] 압축, Time: 0.87 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,14 +1,14 @@
 def solution(msg):
     answer = []
-    dict = {str(chr(i+64)): i for i in range(1,27)}
+    dict = {chr(i+64): i for i in range(1, 27)}
     i = 0
     while i < len(msg):
-        w = msg[i]
-        while (i + 1 < len(msg)) and (w + msg[i+1] in dict):
-            w += msg[i+1]
+        m = msg[i]
+        while i + 1 < len(msg) and m + msg[i+1] in dict:
+            m += msg[i+1]
             i += 1
-        answer.append(dict[w])
+        answer.append(dict[m])
         if i + 1 < len(msg):
-            dict[w + msg[i+1]] = len(dict)+1
+            dict[m+msg[i+1]] = len(dict) + 1
         i += 1
     return answer
