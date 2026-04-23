c98b2812a30eeb702640a5669b2a9bbb4aff5864
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-23 10:54:43 +0900

    [level 1] Title: 실패율, Time: 17.30 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,10 +1,10 @@
 def solution(N, stages):
-    unclear = [0] * (N+2)
+    unclear = [0] * (N+2) # 도달했지만 클리어하지 못한 플레이어의 수
     for stage in stages:
         unclear[stage] += 1
     fails = { }
-    reached = len(stages)
-    for i in range(1, N+1):
+    reached = len(stages) # 스테이지에 도달한 플레이어 수
+    for i in range(1, N+1): # 1번 스테이지부터 N번 스테이지까지
         if unclear[i] == 0:
             fails[i] = 0
         else:
