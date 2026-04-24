fe5740272d04966c4104cb8efceb147271a6345f
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-24 21:55:41 +0900

    [level 2] Title: 주식가격, Time: 25.71 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,12 @@
 def solution(prices):
     stk = [0]
-    n = len(prices)
-    a = [0]*n
-    for i in range(1, n):
+    a = [0] * len(prices)
+    for i in range(1, len(prices)):
         while stk and prices[i] < prices[stk[-1]]:
             j = stk.pop()
             a[j] = i - j
         stk.append(i)
     while stk:
         j = stk.pop()
-        a[j] = n - 1 - j
+        a[j] = len(prices) - 1 - j
     return a
