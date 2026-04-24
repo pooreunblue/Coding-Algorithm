62a45eac7e9ca2fbdd8df38ffcd22d3a382cbbb8
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-04-24 11:16:09 +0900

    [level 2] Title: 방문 길이, Time: 0.42 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,9 +1,10 @@
 def solution(dirs):
     x, y = 0, 0
     path = set()
-    moving = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
+    move = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
     for dir in dirs:
-        nx, ny = x + moving[dir][0], y + moving[dir][1]
+        nx = x + move[dir][0]
+        ny = y + move[dir][1]
         if -5 <= nx <= 5 and -5 <= ny <= 5:
             path.add((x,y,nx,ny))
             path.add((nx,ny,x,y))
