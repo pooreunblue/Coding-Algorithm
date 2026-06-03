4d26aa444f62145c88e82589e7b665d9c4a8de1d
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-06-04 02:16:57 +0900

    [Passed] 점수대 파악하기 (51ms, 17MB)

---
+++ trail1/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,8 @@
+scores = list(map(int, input().split()))
+arr = [0] * 11
+for score in scores:
+    if score == 0:
+        break
+    arr[score//10] += 1
+for i in range(10,0,-1):
+    print(f"{i}0 - {arr[i]}")
