9e37e1a67ba22ec7e4605981cd2abae07828da71
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-06-14 20:42:01 +0900

    [Passed] 중복되지 않는 정수 중 최대 (57ms, 16MB)

--- trail1/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ trail1/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,14 +1,10 @@
 n = int(input())
 nums = list(map(int, input().split()))
-max_v = 0
-is_valid = False
+max_v = -1
 a = [0] * (max(nums)+1)
 for n in nums:
     a[n] += 1
 for n in nums:
     if a[n] == 1 and n > max_v:
         max_v = n
-        is_valid = True
-if is_valid == False:
-    max_v = -1
 print(max_v)
