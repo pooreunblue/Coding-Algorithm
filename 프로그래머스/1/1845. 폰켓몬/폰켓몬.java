6d9d647f77b3fff53cd9c888dd6b4ec230a6f2a0
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-06-18 15:42:34 +0900

    [level 1] Title: 폰켓몬, Time: 3.83 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        System.out.println(Arrays.toString(nums));
         int N = nums.length;
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
