7d7dfed37d7bee231c8f274492f6b16f87d4967b
백승호 <97025669+pooreunblue@users.noreply.github.com>
2026-06-19 11:33:20 +0900

    [level 2] Title: 카펫, Time: 10.21 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -7,13 +7,14 @@ class Solution {
         int total = brown + yellow;
         int w = 0;
         int h = 0;
-        for (int i = 1; i <= total; i++) {
+        for (int i = total; i >= 1; i--) {
             w = i;
             if (total % i == 0) {
                 h = total / w;
                 if ((w-2) * (h-2) == yellow) {
                     answer[0] = w;
                     answer[1] = h;
+                    break;
                 }
             }
         }
